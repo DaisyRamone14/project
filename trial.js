b27b462e024c916cf59c990b04a041ae4a973496
DaisyRamone14 <50694235+DaisyRamone14@users.noreply.github.com>
2020-08-24 21:00:04 +0530

    Create trial.js

---
+++ trial.js
@@ -0,0 +1,12 @@
+alert('hello!');
+function multiply(num1,num2) {
+  let result = num1 * num2;
+  return result;
+}
+multiply(3,4);
+let iceCream = 'chocolate';
+if(iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');    
+} else {
+  alert('Awwww, but chocolate is my favorite...');    
+}
